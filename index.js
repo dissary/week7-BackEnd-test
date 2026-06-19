696d43bf41311f1cbe0f807a1d32dc02e7b300a4
Elijah Chong <dissaryy@gmail.com>
2026-06-19 10:41:48 +0800

    initial commit

--- index.js
+++ index.js
@@ -16,6 +16,85 @@ const pool = new Pool({
   },
 });
 
+async function getPostgresVersion() {
+  const client = await pool.connect();
+  try {
+    const response = await client.query('SELECT version()');
+    console.log(response.rows[0]);
+  } finally {
+    client.release ();
+  }
+}
+
+getPostgresVersion();
+
+//get all from novels
+app.get('/novels', async (req, res) => {
+    const client = await pool.connect();
+    try {
+        const query = "SELECT * FROM novels";
+        const result = await client.query(query);
+        res.json(result.rows);
+    } catch(err) {
+        console.log(err.stack);
+        res.status(500).send("An error occured");
+    } finally {
+        client.release();
+    }
+})
+
+
+//get from novels by id
+app.get('/novels/:id', async(req, res) => {
+  const id = req.params.id;
+  const client = await pool.connect();
+
+  try {
+    const query = "SELECT * FROM novels WHERE id = $1"
+    const result = await client.query(query, [id]);
+
+    if(result.rows.length  === 0) {
+      return res.status(404).send("Not Found");
+    }
+
+    res.json(result.rows);
+  } catch (error) {
+    console.error(error)
+    res.status(500).send("An error occured");
+  } finally {
+    client.release();
+  }
+})
+
+//update from novels by id
+app.put('/novels/:id', async(req, res) => {
+  const id = req.params.id;
+  const updatedData = req.body
+  const client = await pool.connect();
+
+  try {
+    const updateQuery = 'UPDATE novels SET title = $1, author = $2, year_published = $3 WHERE id = $4'
+    const queryData = [updatedData.title, updatedData.author, updatedData.year_published, id]
+    await client.query(updateQuery, queryData)
+
+    // if (!title || !author || !year_published === undefined) {
+    //   return res.status(400).send("Empty Field")
+    // }
+
+
+    // if (year_published !== "number") {
+    //   return res.status(400).send("Empty Field")
+    // }
+
+    res.json({ "status": "success", "message": "Novels updated successfully"})
+  } catch (error) {
+    console.error(error)
+    res.status(500).send("An error occured");
+  } finally {
+    client.release();
+  }
+})
+
 app.get("/", (req, res) => {
   res.sendFile(path.join(__dirname, "index.html"));
 });
